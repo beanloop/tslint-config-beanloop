1400c781a4dd57ce4c637d12f63af15be51d54d4
Jesper Håkansson <jesper@jesperh.se>
2016-12-13 09:58:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# tslint-config-beanloop
+An opinionated tslint config.
+
+## Installation
+
+```sh
+yarn add tslint-config-beanloop
+npm install --save tslint-config-beanloop
+```
+
+## Usage
+
+Set your tslint config to:
+
+```json
+{
+  "extends": [
+    "tslint-config-beanloop"
+  ]
+}
+```
+
+## License
+tslint-config-beanloop is dual-licensed under Apache 2.0 and MIT
+terms.
---
+++ index.js
@@ -0,0 +1,111 @@
+module.exports = {
+    "jsRules": {
+        "class-name": true,
+        "comment-format": [
+            true,
+            "check-space"
+        ],
+        "indent": [
+            true,
+            "spaces"
+        ],
+        "no-duplicate-variable": true,
+        "no-eval": true,
+        "no-trailing-whitespace": true,
+        "no-consecutive-blank-lines": true,
+        "no-unsafe-finally": true,
+        "no-var-keyword": true,
+        "no-default-export": true,
+        "one-line": [
+            true,
+            "check-open-brace",
+            "check-whitespace"
+        ],
+        "quotemark": [
+            true,
+            "single"
+        ],
+        "semicolon": [
+            true,
+            "never"
+        ],
+        "triple-equals": [
+            true,
+            "allow-null-check"
+        ],
+        "variable-name": [
+            true,
+            "ban-keywords"
+        ],
+        "whitespace": [
+            true,
+            "check-branch",
+            "check-decl",
+            "check-operator",
+            "check-separator",
+            "check-type"
+        ]
+    },
+    "rules": {
+        "array-type": [true, "generic"],
+        "class-name": true,
+        "comment-format": [
+            true,
+            "check-space"
+        ],
+        "indent": [
+            true,
+            "spaces"
+        ],
+        "no-eval": true,
+        "no-internal-module": true,
+        "no-trailing-whitespace": true,
+        "no-consecutive-blank-lines": true,
+        "no-unsafe-finally": true,
+        "no-var-keyword": true,
+        "no-default-export": true,
+        "one-line": [
+            true,
+            "check-open-brace",
+            "check-whitespace"
+        ],
+        "ordered-imports": [true, {"import-sources-order": "lowercase-last", "named-imports-order": "lowercase-last"}],
+        "prefer-for-of": true,
+
+        "quotemark": [
+            true,
+            "single"
+        ],
+        "semicolon": [
+            true,
+            "never"
+        ],
+        "trailing-comma": [false, {"multiline": "always", "singleline": "never"}],
+        "triple-equals": [
+            true,
+            "allow-null-check"
+        ],
+        "typedef-whitespace": [
+            true,
+            {
+                "call-signature": "nospace",
+                "index-signature": "nospace",
+                "parameter": "nospace",
+                "property-declaration": "nospace",
+                "variable-declaration": "nospace"
+            }
+        ],
+        "variable-name": [
+            true,
+            "ban-keywords"
+        ],
+        "whitespace": [
+            true,
+            "check-branch",
+            "check-decl",
+            "check-operator",
+            "check-separator",
+            "check-type"
+        ]
+    }
+}
